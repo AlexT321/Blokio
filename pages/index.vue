6d2593ea525ea2f729833b4bb97301c38899be95
alexis tapia <alexist8888@gmail.com>
2022-10-02 16:19:57 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-20 h-20 bg-slate-600">Hello World</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
